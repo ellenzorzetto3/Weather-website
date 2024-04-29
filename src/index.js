97e35f1cf4da4c6236a1fab467ba9abd3fc5c4e3
Ellen Zorzetto <ellen.zorzetto.it@gmail.com>
2024-04-28 22:23:44 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#current-temperature");
-  let temperature = Math.round(response.data.temperature.current);
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = temperature;
+function sayTime(response) {
+  console.log(response.data.temperature.current);
 }
 
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
   let city = searchInputElement.value;
+  cityElement.innerHTML = `${city}`;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiKey = "o3bfb209f55e8951210f40e6476fat3f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(displayTemperature);
+  console.log(apiUrl);
+
+  axios.get(apiUrl).then(sayTime);
 }
 
 function formatDate(date) {
